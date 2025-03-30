be1b06f59dd1d3f98b97106c14eaca1b0a6ad800
Sean Cronin <scronin.dev@gmail.com>
2025-03-30 21:54:21 +0100

    Adding Base Spring Commit
    Adding the intitial Spring Boot Configuration

---
+++ src/test/java/com/scronin/aitken_junior/AitkenJuniorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.scronin.aitken_junior;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AitkenJuniorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
